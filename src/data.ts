c4cce1cef9c7a98725e7f58062eb8fd6c20d10a1
Prak22 <prakhar.s1113@gmail.com>
2022-05-11 02:41:56 +0530

    first commit

---
+++ src/data.ts
@@ -0,0 +1,55 @@
+import { Items } from "./app/ToAll/Models/Items";
+
+export const sample_items: Items[] = [
+  {
+    id:'1',
+    name:'Cumin',
+    price:50.00,
+    imageUrl:'assets/cumin.jpg'
+  },
+  {
+    id:'2',
+    name:'Fenugreek',
+    price:70.00,
+    imageUrl:'assets/fenugreek.jpg'
+  },
+  {
+    id:'3',
+    name:'Pulses',
+    price:25.00,
+    imageUrl:'assets/pulses.jpg'
+  },
+  {
+    id:'4',
+    name:'Millet',
+    price:30.00,
+    imageUrl:'assets/millet.jpg'
+  },
+ 
+  {
+    id:'6',
+    name:'Kiwi',
+    price:180.00,
+    imageUrl:'assets/kiwi.jpg'
+  },
+  {
+    id:'6',
+    name:'Avocado',
+    price:210.00,
+    imageUrl:'assets/avocado.jpg'
+  },
+  {
+    id:'7',
+    name:'Eggs',
+    price:70.00,
+    imageUrl:'assets/egg.jpg'
+  },
+  {
+    id:'8',
+    name:'Olive oil',
+    price:210.00,
+    imageUrl:'assets/olive.jpg'
+  },
+ 
+  
+]
